e9b3ec3dbf5c19370d9c46e086cb8858cf78caf2
YongliuLi <ylli@thoughtworks.com>
2016-07-26 23:33:17 +0800

    [#0] add ky-016 test

---
+++ ky016_test/src/index.js
@@ -0,0 +1,39 @@
+'use strict';
+
+$.ready(function (error) {
+    if (error) {
+        console.log(error);
+        return;
+    }
+    console.log('turnOn LED.');
+    $('#led-r').turnOn();
+	$('#KY-016').turnOn();
+
+	var max_rgb = 255;
+	var abs=function(i){
+		if(i >= 0){
+			return i; 
+		}else{
+			return -1*i;
+		}
+	}
+	var mod=function(i, d){
+		if(i<d){
+			return i;
+		}else{
+			return i - d;
+		}
+	}
+	var cnt=0;
+    setInterval(function(){
+    	cnt = mod(cnt + 1, max_rgb * 2);
+    	var rgb_show = abs(cnt - max_rgb);
+    	$('#KY-016').setRGB([rgb_show,rgb_show,rgb_show]);
+    },10);
+
+});
+
+$.end(function () {
+    $('#led-r').turnOff();
+    $('#KY-016').turnOff();
+});
