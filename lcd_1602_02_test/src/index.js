4eb0b91003f33b0ab921bcd07d79a9b4c04b4b55
YongliuLi <ylli@thoughtworks.com>
2016-07-27 18:43:12 +0800

    [#0] add 1602 test

---
+++ lcd_1602_02_test/src/index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+$.ready(function (error) {
+    if (error) {
+        console.log(error);
+        return;
+    }
+    var cnt = 0;
+    $("#LCD1602-02").turnOn();
+    $("#LCD1602-02").print("Hello World!");
+    $('#led-r').turnOn();
+    $('#CK002').on('push', function() {
+    	cnt = cnt + 1;
+    	$("#LCD1602-02").clear();
+        console.log('Button pushed.');
+        $("#LCD1602-02").print("" + cnt);
+        $('#led-r').turnOn();
+    });
+});
+
+$.end(function () {
+    $('#led-r').turnOff();
+    $("#LCD1602-02").clear();
+    $("#LCD1602-02").turnOff();
+});
